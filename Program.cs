e8dbd4399f94c0a01f8d762affec3a6b6325f870
Graceless <syerdnaswag@gmail.com>
2022-09-28 18:20:36 +0300

    Added program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] array = { "carpe", "diem", "et", "memento", "mori" };
+
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+
+string[] SetArray(string[] arr)
+{
+    int length = arr.Length;
+    string[] result = new string[length];
+    int count = 0;
+    int wordLength = 3;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (arr[i].Length <= wordLength)
+        {
+            result[count] = arr[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+    return result;
+}
+
+string[] resArr = SetArray(array);
+
+System.Console.WriteLine($"[{string.Join(", ", resArr)}]");
